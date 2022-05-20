cb95577e4fffbb044f0a83198586c2029ea79050
FrancescoBasso <frnacescobasso03@gmail.com>
2022-05-20 14:49:28 +0200

    Richiesta coda

--- app/src/main/java/com/example/travelling/MainActivity.java
+++ app/src/main/java/com/example/travelling/MainActivity.java
@@ -2,12 +2,68 @@ package com.example.travelling;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+private Button btnRequest;
+private String url="https://run.mocky.io/v3/86b17ddc-cc9c-465c-8b39-ad04ff144f6c";
+    private Object StringRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRequest = (Button) findViewById(R.id.button);
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getData();
+            }
+        });
+
+
+    }
+
+    private void getData() {
+        final TextView textView = (TextView) findViewById(R.id.text);
+// ...
+
+// Instantiate the RequestQueue.
+        //1 coda richeste
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.google.com";
+
+// Request a string response from the provided URL.
+        //2 richiesta
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+            //se la risposta è corretta
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Toast.makeText(getApplicationContext(),response, Toast.LENGTH_LONG).show();
+                        //textView.setText("Response is: " + response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            // se la risposta è  errata
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+        //4 aggiunta della richesta della coda
+        queue.add(StringRequest);
+
     }
 }
